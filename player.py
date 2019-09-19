b645190b097c8e1979ca326bced7c54c0d46bb65
sybnex <sybnex@gmail.com>
2019-09-19 14:10:40 +0200

    next round

--- player.py
+++ player.py
@@ -18,27 +18,28 @@ class Player:
             card2r = game_state["players"][pId]["hole_cards"][1]["rank"]
             card1s = game_state["players"][pId]["hole_cards"][0]["suit"]
             card2s = game_state["players"][pId]["hole_cards"][1]["suit"]
+
             if card1r == card2r: ranked = True
             if card1s == card2s: suited = True
+
+            rankRange = ("8","9","J","K","Q","A")
+            if card1r in rankRange and card2r in rankRange: self.goodCards = True
+
         except:
             print("ERROR ON CARD CALC")
 
-        rankRange = ("9","J","K","Q","A")
-        if suited and card1r in rankRange and card2r in rankRange: 
-            goodCards = 1
-            bet += 100
-        elif card1r in rankRange and card2r in rankRange: 
-            goodCards = 1
-            bet += 100
+        if suited and self.goodCards: bet += 200
+        elif self.goodCards:          bet += 100
+        elif ranked or suited:        bet += 50
+        elif game_state["current_buy_in"] > 250: return 0
+        else:                                    return 0
 
-        if ranked or suited:
-            bet += 50
-            #return game_state["players"][pId]["stack"]
-
-        if   response < 0: return 1
+        if   response < 0: return 0
         else:              return response
 
-    def showdown(self, game_state):
+        #return game_state["players"][pId]["stack"] # allIn
+
+def showdown(self, game_state):
         print(str(game_state))
         pass
 
