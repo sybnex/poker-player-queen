16be503b7a3ee7b3c00c6c251a9f9fde777846b4
sybnex <sybnex@gmail.com>
2019-09-19 11:38:15 +0200

    next round

--- player.py
+++ player.py
@@ -11,18 +11,19 @@ class Player:
         bet  = game_state["players"][pId]["bet"]
         call = game_state["current_buy_in"]
 
-        equal = False
+        ranked = False
+        suited = False
         try:
             card1r = game_state["players"][pId]["hole_cards"][0]["rank"]
             card2r = game_state["players"][pId]["hole_cards"][1]["rank"]
             card1s = game_state["players"][pId]["hole_cards"][0]["suit"]
             card2s = game_state["players"][pId]["hole_cards"][1]["suit"]
-            if card1r == card2r: equal = True
-            if card1s == card2s: equal = True
+            if card1r == card2r: ranked = True
+            if card1s == card2s: suited = True
         except:
-            equal = False
+            print("ERROR ON CALC")
 
-        if game_state["round"] == 3 or equal:
+        if game_state["round"] == 2 or suited:
             return game_state["players"][pId]["stack"]
 
         response = call - bet + game_state["minimum_raise"]
