d32c16712cb405cd91027c73ecc4cc922000dfa0
sybnex <sybnex@gmail.com>
2019-09-19 10:12:37 +0200

    asdded min raise

--- player.py
+++ player.py
@@ -2,13 +2,18 @@
 class Player:
     VERSION = "Queen Python folding player"
 
+    def bet_request(self, game_state):
+        return self.betRequest(game_state)
+
     def betRequest(self, game_state):
 
-        player = game_state["players"]
+        players = game_state["players"]
         bet = 0
-        for i in player: 
-            bet += player[i]["bet"]
+        print(players)
+        for player in players: 
+            bet += players[player]["bet"]
 
+        
         call = game_state["current_buy_in"]
 
         return call - bet + 1
